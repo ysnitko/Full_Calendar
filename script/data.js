98a033a826fc23df3b7f35c1d1252f0a2ac3f35a
yuri_sn <yuri.snitko@gmail.com>
2023-05-03 20:50:32 +0300

    js task

---
+++ script/data.js
@@ -0,0 +1,23 @@
+const month = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const daysOfWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
